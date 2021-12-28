ff6e4b004d3d023477aa110f9ddae8739f0e339d
kodluyoruz <teoman.yanmaz@hotmail.com>
2021-12-28 03:33:55 +0300

    updated loader

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -5,8 +5,8 @@ db.once("open", () => {
   console.log("Connected to Mongodb");
 });
 
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI, {
+const connectDB = () => {
+  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
